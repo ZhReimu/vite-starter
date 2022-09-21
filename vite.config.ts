7db1a7d5b2a114fe255789686fe387e8151f7b4f
博麗靈夢 <1725338233@qq.com>
2022-09-21 20:05:26 +0800

    修改 autoimport 文件位置

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     AutoImport({
       resolvers: [ElementPlusResolver()],
       imports: ["vue", "vue-router"], // 自动导入vue和vue-router相关函数
-      dts: "src/auto-import.d.ts" // 生成 `auto-import.d.ts` 全局声明
+      dts: "auto-import.d.ts" // 生成 `auto-import.d.ts` 全局声明
     }),
     Components({
       resolvers: [ElementPlusResolver()],
